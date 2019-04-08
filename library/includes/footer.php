431cb23f6921d2cb78f5e7bd1428d55eb1f03152
“thariii” <tharikalakshani95@gmail.com>
2019-04-08 13:32:35 +0530

    first commit

--- library/includes/footer.php
+++ library/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2019 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : ITP_GROUP_16</a> 
+                   &copy; 2019 Online Library Management System |<a href="https://facebook.com/" target="_blank"  > Designed by : ITP_GROUP_16</a> 
                 </div>
 
             </div>
